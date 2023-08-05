bd9e07098e1e65c271edd05b20a63c91e31e4a97
Tiago Costa <tdcosta100@gmail.com>
2023-08-05 03:29:44 -0300

    Avoid to create a RunLoop instance for every tile request

--- TileServerGL/ServeData.cs
+++ TileServerGL/ServeData.cs
@@ -1,5 +1,6 @@
 ﻿using Mapbox.Vector.Tile;
 using MaplibreNative;
+using System.Collections.Concurrent;
 using System.IO.Compression;
 using System.Net.Mime;
 using System.Text;
@@ -8,6 +9,182 @@ using System.Text.Json.Nodes;
 
 namespace TileServerGL
 {
+    public class FileProvider : IDisposable
+    {
+        private Thread _FileProviderThread;
+        private bool _Disposed = false;
+        private RunLoop? _RunLoop = null;
+        private FileSource? _FileSource = null;
+        private BlockingCollection<Action<(RunLoop RunLoop, FileSource FileSource)>> _FileRequestQueue = new();
+        private CancellationTokenSource _CancellationTokenSource = new();
+
+        public FileProvider(Func<(RunLoop RunLoop, FileSource FileSource)> initializer)
+        {
+            _FileProviderThread = new Thread(() =>
+            {
+                Thread.CurrentThread.Name = $"FileProvider {Environment.CurrentManagedThreadId}";
+
+                (_RunLoop, _FileSource) = initializer();
+
+                try
+                {
+                    while (_FileRequestQueue.TryTake(out Action<(RunLoop RunLoop, FileSource FileSource)>? action, Timeout.Infinite, _CancellationTokenSource.Token))
+                    {
+                        try
+                        {
+                            action((_RunLoop, _FileSource));
+                        }
+                        finally
+                        {
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                }
+
+                _RunLoop.Dispose();
+            });
+
+            if (OperatingSystem.IsWindows())
+            {
+                _FileProviderThread.SetApartmentState(ApartmentState.STA);
+            }
+
+            _FileProviderThread.Start();
+        }
+
+        public void AddToFileRequestQueue(Action<(RunLoop RunLoop, FileSource FileSource)> action)
+        {
+            _FileRequestQueue.Add(action);
+        }
+
+        public Task InvokeAsync(Action<(RunLoop RunLoop, FileSource FileSource)> action)
+        {
+            var taskCompletionSource = new TaskCompletionSource();
+
+            AddToFileRequestQueue(elements =>
+            {
+                try
+                {
+                    action((elements.RunLoop, elements.FileSource)!);
+                    taskCompletionSource.SetResult();
+                }
+                catch (Exception ex)
+                {
+                    taskCompletionSource.SetException(ex);
+                }
+            });
+
+            return taskCompletionSource.Task;
+        }
+
+        public void Invoke(Action<(RunLoop RunLoop, FileSource FileSource)> action)
+        {
+            try
+            {
+                InvokeAsync(action).Wait();
+            }
+            catch (AggregateException ex)
+            {
+                throw ex.InnerException!;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!_Disposed)
+            {
+                _Disposed = true;
+                _CancellationTokenSource.Cancel();
+                _CancellationTokenSource.Dispose();
+                _FileProviderThread.Join();
+            }
+        }
+    }
+
+    public class FileProviderPool
+    {
+        #region Private fields
+        private readonly object _Lock = new();
+        private readonly int _MinFileProviders;
+        private readonly int _MaxFileProviders;
+        private readonly Func<(RunLoop RunLoop, FileSource FileSource)> _FileProviderInitializer;
+        private readonly BlockingCollection<FileProvider> _IdleFileProviders = new(new ConcurrentBag<FileProvider>());
+        private int _TotalFileProviders;
+        private readonly Timer _RemoveFileProviderTimer;
+        #endregion
+
+        #region Constructor
+        public FileProviderPool(int minFileProviders, int maxFileProviders, Func<(RunLoop RunLoop, FileSource FileSource)> fileProviderInitializer)
+        {
+            _MinFileProviders = minFileProviders;
+            _MaxFileProviders = maxFileProviders;
+            _FileProviderInitializer = fileProviderInitializer;
+            _RemoveFileProviderTimer = new Timer(ReduceFileProviders, null, Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+
+            _TotalFileProviders = 0;
+
+            lock (_Lock)
+            {
+                Task.WaitAll(
+                    Enumerable.Range(0, _MinFileProviders)
+                    .Select(_ => Task.Run(() => _IdleFileProviders.Add(new FileProvider(_FileProviderInitializer))))
+                    .ToArray()
+                );
+
+                _TotalFileProviders = _MinFileProviders;
+            }
+        }
+        #endregion
+
+        #region Private methods
+        private void ReduceFileProviders(object? state)
+        {
+            lock (_Lock)
+            {
+                if (_IdleFileProviders.Count == _TotalFileProviders)
+                {
+                    Task.WaitAll(
+                        Enumerable.Range(0, _TotalFileProviders - _MinFileProviders)
+                        .Select(_ => Task.Run(() => _IdleFileProviders.Take().Dispose()))
+                        .ToArray()
+                    );
+
+                    _TotalFileProviders = _MinFileProviders;
+                }
+            }
+        }
+        #endregion
+
+        #region Public methods
+        public FileProvider Acquire()
+        {
+            lock (_Lock)
+            {
+                _RemoveFileProviderTimer.Change(Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+
+                if (!_IdleFileProviders.Any() && _TotalFileProviders < _MaxFileProviders)
+                {
+                    _TotalFileProviders++;
+                    return new FileProvider(_FileProviderInitializer);
+                }
+            }
+
+            return _IdleFileProviders.Take();
+        }
+
+        public void Release(FileProvider fileProvider)
+        {
+            lock (_Lock)
+            {
+                _IdleFileProviders.Add(fileProvider);
+                _RemoveFileProviderTimer.Change(TimeSpan.FromSeconds(30), Timeout.InfiniteTimeSpan);
+            }
+        }
+        #endregion
+    }
+
     public class ServeData
     {
         private class LowerCaseNamingPolicy : JsonNamingPolicy
@@ -18,6 +195,7 @@ namespace TileServerGL
         public static void Init(Configuration configuration, IApplicationBuilder applicationBuilder, IEndpointRouteBuilder endpointRouteBuilder, string routePrefix)
         {
             var fileSource = FileSourceManager.GetFileSource(FileSourceType.Mbtiles, ResourceOptions.Default());
+            var fileProviderPool = new FileProviderPool(0, 16, () => (new RunLoop(), fileSource));
             var lowerCaseNamingPolicy = new LowerCaseNamingPolicy();
             var serveBounds = new[]
             {
@@ -60,16 +238,24 @@ namespace TileServerGL
 
                 Response? response = null;
 
-                using (var runLoop = new RunLoop())
+                var fileProvider = fileProviderPool.Acquire();
+
+                try
                 {
-
-                    var request = fileSource.Request(Resource.Tile(string.Format("mbtiles://{0}?tile={{z}}/{{x}}/{{y}}.{1}", Path.Combine(configuration.Options!.Paths!.MBTiles, data.MBTiles), tileJSONFormat), 1.0f, x, y, z, Resource.TilesetScheme.XYZ), res =>
+                    fileProvider.Invoke(elements =>
                     {
-                        response = res;
-                        runLoop.Stop();
-                    });
+                        var request = elements.FileSource.Request(Resource.Tile(string.Format("mbtiles://{0}?tile={{z}}/{{x}}/{{y}}.{1}", Path.Combine(configuration.Options!.Paths!.MBTiles, data.MBTiles), tileJSONFormat), 1.0f, x, y, z, Resource.TilesetScheme.XYZ), res =>
+                        {
+                            response = res;
+                            elements.RunLoop.Stop();
+                        });
 
-                    runLoop.Run();
+                        elements.RunLoop.Run();
+                    });
+                }
+                finally
+                {
+                    fileProviderPool.Release(fileProvider);
                 }
 
                 if (response!.Error != null)
